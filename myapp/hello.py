7c5fa0eb4bbc49400a633a20ea17a6e72343f38a
jamsonsor <jamsons93@gmail.com>
2023-09-18 23:59:27 -0400

    added def for bye in python app

--- myapp/hello.py
+++ myapp/hello.py
@@ -3,5 +3,9 @@ import fire
 def hello(name="World"):
   return "Hello %s!" % name
 
+def bye(name="User"):
+  return "Bye %s!" % name
+
 if __name__ == '__main__':
   fire.Fire(hello)
+  fire.Fire(bye)
